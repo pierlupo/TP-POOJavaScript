e3370514fa1fe4bc629405aec2980150abb7aa76
Kraimps Pierre <pier_lupo@laposte.net>
2023-01-30 11:48:28 +0100

    Suite

--- TP-ListeDeContacts/LesClasses/Contact.js
+++ TP-ListeDeContacts/LesClasses/Contact.js
@@ -1,16 +1,23 @@
 export default class Contact {
-    constructor(sex, name, firstname, birthDate, tel, email){
-        this.sex = sex;
-        this.name = name;
-        this.firstname = firstname;
-        this.birthDate = birthDate;
-        this.tel = tel;
-        this.email = email;
+    constructor(genre, nom, prenom, dateNaissance, phoneTel, mail){
+        this.genre = genre;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.phoneTel = phoneTel;
+        this.mail = mail;
 
     }
 
     Display(){
-       return `${this.sex} ${this.name} ${this.firstname} ${this.birthDate} ${this.tel} ${this.email}. `;
+       return `${this.genre} ${this.nom} ${this.prenom} ${this.dateNaissance} ${this.phoneTel} ${this.mail}. `;
     }
-    
+    /*getter et setter */
+    // set nom(n){
+    //     this.nom = n;
+    // }
+    // get nom(){
+    // return this.nom; 
+    // }
 }
+
--- TP-ListeDeContacts/LesClasses/Ihm.js
+++ TP-ListeDeContacts/LesClasses/Ihm.js
@@ -17,16 +17,19 @@ export class Ihm {
 
     ajouter() {
         //Récupérer les champs et créer un contact:
-        const genre = this.formulaire.queryselector("input[name='genre']").value;
-        const nom= this.formulaire.queryselector("input[name='nom']").value;
-        const prenom= this.formulaire.queryselector("input[name='prenom']").value;
-        const phoneTel= this.formulaire.queryselector("input[name='phoneTel']").value;
-        const dateNaissance= this.formulaire.queryselector("input[name='dateNaissance']").value;
-        const mail= this.formulaire.queryselector("input[name='mail']").value;
+        const genre = this.formulaire.querySelector("input[name='genre']").value;
+        const nom= this.formulaire.querySelector("input[name='nom']").value;
+        const prenom= this.formulaire.querySelector("input[name='prenom']").value;
+        const phoneTel= this.formulaire.querySelector("input[name='phoneTel']").value;
+        const dateNaissance= this.formulaire.querySelector("input[name='dateNaissance']").value;
+        const mail= this.formulaire.querySelector("input[name='mail']").value;
+        //const resetInput = this.formulaire.querySelectorAll('.form-control').value;
 
         const contact = new Contact(genre, nom, prenom, phoneTel, dateNaissance,mail);
         this.contacts.push(contact);
         this.afficher(contact);
+        //resetInput.innerHTML = "";
+        
     }
 
     afficher(c){
@@ -42,17 +45,17 @@ export class Ihm {
         `;
 
         //this.contacts = [];
-        this.tableauHTMLResult.innerHTML = "";
-        this.contacts.forEach(c => {
-            this.tableauHTMLResult.innerHTML += `<tr>
-            <td>${c.genre}</td>
-            <td>${c.nom}</td>
-            <td>${c.prenom}</td>
-            <td>${c.phoneTel}</td>
-            <td>${c.dateNaissance}</td>
-            <td>${c.mail}</td>
-            </tr>
-            `;
-        })
+        // this.tableauHTMLResult.innerHTML = "";
+        // this.contacts.forEach(c => {
+        //     this.tableauHTMLResult.innerHTML += `<tr>
+        //     <td>${c.genre}</td>
+        //     <td>${c.nom}</td>
+        //     <td>${c.prenom}</td>
+        //     <td>${c.phoneTel}</td>
+        //     <td>${c.dateNaissance}</td>
+        //     <td>${c.mail}</td>
+        //     </tr>
+        //     `;
+        // })
     }
 }
