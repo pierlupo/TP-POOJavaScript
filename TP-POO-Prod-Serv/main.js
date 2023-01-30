3575c470cdac92301379d64ba90c1583104cee5f
Kraimps Pierre <pier_lupo@laposte.net>
2023-01-30 16:13:28 +0100

    suite

--- TP-POO-Prod-Serv/main.js
+++ TP-POO-Prod-Serv/main.js
@@ -5,25 +5,25 @@ import  Ihm  from "./Classes/Ihm.js";
 const form = document.querySelector('#formContact');
 const result = document.querySelector('#result');
 const result1 = document.querySelector('#result1');
-let article0 = document.querySelector("input[name='article']:checked").value;
-let article1 = document.getElementsById('parentRadio');
-const inputDomaine = document.querySelector('#domain');
-const inputStok = document.querySelector('#stok');
+//let article0 = document.querySelector("input[name='article']:checked").value;
+//let article1 = document.getElementsById('parentRadio');
+//const inputDomaine = document.querySelector('#domain');
+//const inputStok = document.querySelector('#stok');
 // const ihmProd = new Ihm(form, result);
 // const ihmServ = new Ihm(form, result1);
 
-this.formulaire.addEvenListener("change", ()=>{
+// this.formulaire.addEvenListener("change", ()=>{
 
-    if(article0 == "Produit") {
+//     if(article0 == "Produit") {
 
-        inputDomaine.style.display = "none";
+//         inputDomaine.style.display = "none";
         
-        } else if (article0 == "Service") {
+//         } else if (article0 == "Service") {
         
-        inputStok.style.display = "none";
+//         inputStok.style.display = "none";
         
-        }
-})
+//         }
+// })
 
 
 const ihm = new Ihm(form, result, result1);
